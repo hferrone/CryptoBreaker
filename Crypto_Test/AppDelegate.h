2f2f70835e02282c1098094105f1f16748d4f61b
Harrison Ferrone <hferrone@gmail.com>
2014-08-22 15:17:48 -0500

    Initial Commit

---
+++ Crypto_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Crypto_Test
+//
+//  Created by Harrison Ferrone on 8/22/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Crypto_Test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Crypto_Test
+//
+
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
